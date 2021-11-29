10bc105b9ded7e388648256c7ddd52f43acdbb55
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-30 02:11:01 +0700

    -

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -19,7 +19,7 @@
                     <div
                       class="text-h4 q-pb-md text-blue-8 text-center text-weight-bolder" style="margin-top:100px"
                     >
-                      Logincvbnm,dddddd./
+                      Loginthth
                     </div>
                     <q-form
                       @submit="onSubmit"
